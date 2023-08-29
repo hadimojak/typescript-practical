da8dbb27d75756f98e692a9d970c02dda66d01b1
h.arbabi <hadimojak@gmail.com>
2023-08-29 18:24:21 +0330

    literal types

--- app.ts
+++ app.ts
@@ -1,29 +1,22 @@
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: "ali",
-//   age: 32,
-//   hobbies: ["sports", "studying"],
-//   role: [1, "admin"],
-// };
-
-enum Role {
-  admin = "ADMIN",
-  user = 0,
-  reporter,
+function add(
+  n1: number | string,
+  n2: number | String,
+  resultType: "number" | "string"
+) {
+  if (resultType === "number") {
+    return `${resultType} is  ${+n1 + +n2}`;
+  }
+  if (resultType === "string") {
+    return `${resultType} is ${n1.toString() + n2.toString()}`;
+  }
 }
 
-const person = {
-  name: "ali",
-  age: 32,
-  hobbies: ["sports", "studying"],
-  role: Role.admin,
-};
+const num1 = 7;
+const num2 = 4;
 
-// let favArr: string[];
+const result = add(num1, num2, 'number');
+console.log(result);
 
-// for (const hobby of person.hobbies) console.log(hobby);
-console.log(person.role);
+console.log(add("hellow", " there", "string"));
+
+console.log(add("6", "5", "number"));
