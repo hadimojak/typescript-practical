f2e992e8e50bfc686296c3e22f1b404b5e40bdaa
h.arbabi <hadimojak@gmail.com>
2023-08-30 17:21:59 +0330

    all Types

--- app.ts
+++ app.ts
@@ -1,22 +1,3 @@
-function add(
-  n1: number | string,
-  n2: number | String,
-  resultType: "number" | "string"
-) {
-  if (resultType === "number") {
-    return `${resultType} is  ${+n1 + +n2}`;
-  }
-  if (resultType === "string") {
-    return `${resultType} is ${n1.toString() + n2.toString()}`;
-  }
-}
+const button = document.querySelector("button") as HTMLButtonElement;
 
-const num1 = 7;
-const num2 = 4;
-
-const result = add(num1, num2, 'number');
-console.log(result);
-
-console.log(add("hellow", " there", "string"));
-
-console.log(add("6", "5", "number"));
+button.addEventListener('click',()=>console.log('clicked'))
