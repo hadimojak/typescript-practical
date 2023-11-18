766b6eebc253a5ef401a5504685666b6b7838fd4
h.arbabi <hadimojak@gmail.com>
2023-11-18 17:33:23 +0330

    guard

--- src/app.ts
+++ src/app.ts
@@ -23,3 +23,57 @@ type Combinable = string | number;
 type Numberic = number | boolean;
 
 type Univarsal = Combinable & Numberic;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  } else return a + b;
+}
+
+type UnknownEmployee = Emloyee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("name :", emp.name);
+  if ("privileges" in emp) {
+    console.log("privilege :", emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("privilege :", emp.startDate);
+  }
+}
+
+printEmployeeInformation(emp1);
+
+class Car {
+  drive() {
+    console.log("driving");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("driving a truck");
+  }
+
+  loadCargo(amount: number) {
+    console.log("carring :", amount);
+  }
+}
+
+type Vhicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vhicle: Vhicle) {
+  vhicle.drive();
+  // if ("loadCargo" in vhicle) {
+  //   vhicle.loadCargo(5000);
+  // }
+  if (vhicle instanceof Truck) {
+    vhicle.loadCargo(5000);
+  }
+}
+
+useVehicle(v2);
+useVehicle(v1);
