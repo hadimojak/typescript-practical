1c01cf0253d26e2a51699e4fef9db897cb67790a
hadi arbabi <hadimojak@gmail.com>
2024-06-06 18:33:30 +0330

    core types

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-function add(n1: number, n2: number) {
-  if (typeof n1 !== "number" || typeof n2 !== "number") {
-    throw new Error("incorrect input ");
-  }
-  return n1 + n2;
-}
-
-console.log(add(4, 5));
