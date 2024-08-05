f2aa02777018b9002c422f7a98acb32383057e0d
hadmojak <hadimojak@gmail.com>
2024-08-05 11:53:11 +0330

    debugger

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ const divEl = document.getElementById("app") as HTMLDivElement;
 divEl.innerHTML = "<p>hellow sag</p>";
 
 const btn = document.getElementById("button") as HTMLButtonElement;
+function clickHandler(message: string) {
+  console.log(message + ": clicked");
+}
 
-btn.addEventListener("click", (val) => {
-  console.log(val.target);
-});
+btn.addEventListener("click", clickHandler.bind(null, "there"));
