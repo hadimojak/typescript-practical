6cd81283390b490696ddea89de19463e1f1e8104
hadmojak <hadimojak@gmail.com>
2024-09-11 17:50:07 +0330

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+
 type Admin = {
   name: string;
   privileges: string[];
@@ -30,16 +31,19 @@ type Numeric = number | boolean;
 
 type Universal = Combinale & Numeric;
 
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: number): number;
+function add(a: number, b: string): string;
 function add(a: Combinale, b: Combinale) {
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
   }
   return a + b;
 }
-
-// console.log(add(3, 4));
-// console.log(add(3, "4"));
-// console.log(add("qweqwe", "asdasd"));
+const result = add(3, 4);
+const result1 = add(3, "4");
+const result2 = add("qweqwe", "asdasd");
 
 function printEmployeeInformation(emp: UnknownEmloyee) {
   console.log("name: " + emp.name);
@@ -47,7 +51,7 @@ function printEmployeeInformation(emp: UnknownEmloyee) {
   if ("startDate" in emp) console.log("startDate: " + emp.startDate);
 }
 
-printEmployeeInformation(e2);
+// printEmployeeInformation(e2);
 
 class Car {
   drive() {
@@ -74,5 +78,67 @@ function useVehicle(vehicle: Vehicle) {
   if (vehicle instanceof Truck) vehicle.loadCargo(1500);
 }
 
-useVehicle(v1)
-useVehicle(v2)
+// useVehicle(v1)
+// useVehicle(v2)
+
+interface Bird {
+  type?: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type?: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(anima: Animal): string | undefined {
+  let speed;
+  switch (anima.type) {
+    case "bird":
+      speed = "moving with speed " + anima.flyingSpeed;
+      break;
+    case "horse":
+      speed = "moving with speed " + anima.runningSpeed;
+      break;
+
+    default:
+      break;
+  }
+
+  return speed;
+}
+
+const horse: Horse = {
+  runningSpeed: 140,
+  type: "horse",
+};
+
+// console.log(moveAnimal(horse));
+
+// const para = <HTMLInputElement>document.getElementById("inpt");
+const para = document.getElementById("inpt");
+
+if (para) {
+  (para as HTMLInputElement).value = "aaaaa";
+}
+
+interface ErrorContainer {
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: "not valid email",
+  username: "must be start with capital",
+};
+
+const fetchedUserData = {
+  id: "uuid",
+  name: "max",
+  job: { title: "CEO", description: "halishop company" },
+};
+
+
+
+console.log(fetchedUserData?.job?.title);
