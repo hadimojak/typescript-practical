6f5ce1196fa9aee9ac693474d876562a39a456a9
hadmojak <hadimojak@gmail.com>
2024-09-24 17:01:04 +0330

    genereic first commit

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
 const names: Array<string> = ["ali", "max"];
 
-console.log(names);
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("done done");
+  }, 3000);
+});
+
+promise.then((data) => {
+  console.log(data);
+});
