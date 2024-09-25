a55ac2db66895f2bb0f095cf0d0c5a4fa330b186
hadmojak <hadimojak@gmail.com>
2024-09-25 16:37:43 +0330

    generics

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
-const names: Array<string> = ["ali", "max"];
+// const names: Array<string> = ["ali", "max"];
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("done done");
-  }, 3000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("done done");
+//   }, 3000);
+// });
 
-promise.then((data) => {
-  console.log(data);
-});
+// promise.then((data) => {
+//   console.log(data);
+// });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ age: 32 }, { name: "asd" });
+
+console.log(mergedObj);
