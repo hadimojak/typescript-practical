041a137d7fed3fae269f9cc304f76e56a313fcd0
hadmojak <hadimojak@gmail.com>
2024-10-01 19:19:15 +0330

    utitlity types

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function ceateCurseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  const courseGoal: Partial<CourseGoal> = {};
+
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["max", "sports"];
+// names.push("manu");
+// names.pop();
