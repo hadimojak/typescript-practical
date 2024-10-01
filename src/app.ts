714f2d7e7195e58aca66e6eca32b3645be0201cd
hadmojak <hadimojak@gmail.com>
2024-10-01 15:58:47 +0330

    generic funtions

--- src/app.ts
+++ src/app.ts
@@ -16,4 +16,30 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ age: 32 }, { name: "asd" });
 
-console.log(mergedObj);
+// console.log(mergedObj);
+
+interface lengthy {
+  length: number;
+}
+function countAndPrint<T extends lengthy>(element: T): [T, string] {
+  let description = "Got no value";
+  if (element.length === 1) {
+    description = "got 1 element";
+  } else if (element.length > 1) {
+    description = "got " + element.length + " elements";
+  }
+  return [element, description];
+}
+
+// console.log(countAndPrint("hi there"));
+// console.log(countAndPrint([2, 3, 4]));
+// console.log(countAndPrint({ name: "hadi" }));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
+
+console.log(extractAndConvert({ name: "ali" }, "name"));
