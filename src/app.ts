314f8d696b04d721c640657a5c8710ab5e5b4624
h.arbabi <hadimojak@gmail.com>
2024-10-27 18:38:00 +0330

    decorator-commit

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,83 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+//#region class decorator
 function Logger(logName: string) {
-  console.log("logger");
-
-  return function (constructor: Function) {
+  return function (_: Function) {
     console.log("1- logging... " + logName);
-    // console.log(constructor);
   };
 }
 
 function withTemplate(template: string, hookId: string) {
-  console.log("template");
-
-  return function (constructor: any) {
-    const p = new constructor();
-    console.log("2");
-
-    const divElement = document.getElementById(hookId) as HTMLDivElement;
-    divElement.innerHTML = template;
-    (document.querySelector("p") as HTMLParagraphElement).innerHTML = p.name;
+  return function <T extends { new (...args: any[]): { name: string } }>(constructor1: T) {
+    return class extends constructor1 {
+      constructor(..._: any[]) {
+        super();
+        const ULElement = document.getElementById(hookId) as HTMLUListElement;
+        // divElement.innerHTML = template;
+        (document.querySelector("ul") as HTMLUListElement).innerHTML = this.name;
+      }
+    };
   };
 }
 
 @Logger("warn")
 @withTemplate(`<p>hellow decorator</p>`, "app")
 class Person {
-  name = "max";
+  name = "maxii";
 
   constructor() {
     console.log("creating person");
   }
 }
 
-// const person = new Person();
+new Person()
 
-// console.log(person);
+// //#region property decorator
+// function Log(target: any, propertyName: string | symbol) {
+//   console.log("1 -property decorator...", target, propertyName);
+// }
+
+// //#region accessor decorator
+// function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+//   console.log("2 -accessor decorator");
+//   console.log(target);
+//   console.log(name);
+//   console.log(descriptor);
+// }
+
+// //#region method decorator
+// function Log3(target: any, name: string | symbol, descriptor: PropertyDescriptor) {
+//   console.log("3- method decorator");
+//   console.log(target);
+//   console.log(name);
+//   console.log(descriptor);
+// }
+
+// //#region parameter decorator
+// function Log4(target: any, name: string | symbol, index: number) {
+//   console.log("4- parameter decorator");
+//   console.log(target);
+//   console.log(name);
+//   console.log(index);
+// }
+
+// class Product {
+//   @Log
+//   title: string;
+//   private _price: number;
+
+//   @Log2
+//   set price(val: number) {
+//     if (val > 0) this._price = val;
+//     else throw new Error("val is not valid price number");
+//   }
+
+//   constructor(t: string, p: number) {
+//     this.title = t;
+//     this._price = p;
+//   }
+
+//   @Log3
+//   getPriceWithTax(@Log4 tax: number) {
+//     return this._price * (1 + tax);
+//   }
+// }
