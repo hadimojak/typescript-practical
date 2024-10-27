b672669aec52dacf4504c01aba5519762a1a4144
h.arbabi <hadimojak@gmail.com>
2024-10-27 15:42:12 +0330

    decorator-first-commit

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,36 @@
-import { Enforcer, newEnforcer } from "casbin";
-import { SequelizeAdapter } from "casbin-sequelize-adapter";
-import path from "path";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+function Logger(logName: string) {
+  console.log("logger");
 
-class Casbin {
-  private static enforcer: Enforcer;
+  return function (constructor: Function) {
+    console.log("1- logging... " + logName);
+    // console.log(constructor);
+  };
+}
 
-  private static async setup(): Promise<void> {
-    // Initialize a Sequelize adapter and use it in a Node-Casbin enforcer:
-    // The adapter can not automatically create database.
-    // But the adapter will automatically and use the table named "casbin_rule".
-    // The second boolean argument: autoCreateTable determines whether the adapter will automatically create the "casbin_rule" table.
-    // ORM should not create databases automatically.
-    console.log("heerree");
+function withTemplate(template: string, hookId: string) {
+  console.log("template");
 
-    const a = await SequelizeAdapter.newAdapter(
-      {
-        host: "localhost",
-        port: 5432,
-        username: "postgres",
-        password: "123456",
-        database: "test",
-        dialect: "postgres",
-        logging: false,
-      },
-      true
-    );
+  return function (constructor: any) {
+    const p = new constructor();
+    console.log("2");
 
-    const modelPath = path.join(__dirname, "./", "model.conf");
-    console.log(modelPath);
+    const divElement = document.getElementById(hookId) as HTMLDivElement;
+    divElement.innerHTML = template;
+    (document.querySelector("p") as HTMLParagraphElement).innerHTML = p.name;
+  };
+}
 
-    this.enforcer = await newEnforcer(modelPath, a);
-    await this.enforcer.loadPolicy();
+@Logger("warn")
+@withTemplate(`<p>hellow decorator</p>`, "app")
+class Person {
+  name = "max";
 
-    // console.log({ a });
-
-    // const e = await this.enforcer.newEnforcer("examples/rbac_model.conf", a);
-
-    // Check the permission.
-    // e.enforce("alice", "data1", "read");
-
-    // Modify the policy.
-    // await e.addPolicy(...);
-    // await e.removePolicy(...);
-
-    // Save the policy back to DB.
-    // await e.savePolicy();
-  }
-
-  static async initializeEnforcer(): Promise<void> {
-    await this.setup();
-
-    // Now enforcer is guaranteed to be initialized
-    if (!this.enforcer) {
-      throw new Error("Enforcer setup failed.");
-    }
+  constructor() {
+    console.log("creating person");
   }
 }
 
-Casbin.initializeEnforcer();
+// const person = new Person();
+
+// console.log(person);
