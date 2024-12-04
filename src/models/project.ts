2f15223eb3ac1f15411508699e066521490747e7
hadi arbabi <hadimojak@gmail.com>
2024-12-04 12:27:08 +0330

    third party

--- src/models/project.ts
+++
@@ -1,9 +0,0 @@
-export enum ProjectStatus {
-  Active,
-  Finished,
-}
-//#region Project
-export class Project {
-  constructor(public id: string, public title: string, public descrption: string, public people: number, public status: ProjectStatus) {}
-}
-//#endregion Project
