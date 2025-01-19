eebef18e6b0fd6bfb2f4df96d8dd72ac4b4e7abb
hadi arbabi <hadimojak@gmail.com>
2025-01-19 18:29:15 +0330

    map

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 const form = <HTMLFormElement>document.querySelector("form");
 const addressInput = <HTMLInputElement>document.getElementById("address");
 
+const LOCATIONIQ_KEY = "pk.49f671cd5434809527981baab89b28ad";
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
+
+  const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+  fetch('https://us1.locationiq.com/v1/search/structured?street=30%20metri%20nirooye%20havayi&city=tehran&county=iran&key=pk.49f671cd5434809527981baab89b28ad', options)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+
   console.log({ enteredAddress });
 }
 
