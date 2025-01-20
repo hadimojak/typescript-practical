3df24a1fe0cf2ecbf008a410fe4e29da8d9cbba5
hadi arbabi <hadimojak@gmail.com>
2025-01-20 18:13:57 +0330

    map finished

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,45 @@
+// import * as L from "leaflet";
+
 const form = <HTMLFormElement>document.querySelector("form");
-const addressInput = <HTMLInputElement>document.getElementById("address");
+const countryInput = <HTMLInputElement>document.getElementById("country");
+const cityInput = <HTMLInputElement>document.getElementById("city");
+const street1Input = <HTMLInputElement>document.getElementById("street1");
 
 const LOCATIONIQ_KEY = "pk.49f671cd5434809527981baab89b28ad";
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
-  const enteredAddress = addressInput.value;
+  const countryName = countryInput.value;
+  const cityName = cityInput.value;
+  const street1Name = street1Input.value;
 
-  const options = {method: 'GET', headers: {accept: 'application/json'}};
+  const options = { method: "GET", headers: { accept: "application/json" } };
 
-  fetch('https://us1.locationiq.com/v1/search/structured?street=30%20metri%20nirooye%20havayi&city=tehran&county=iran&key=pk.49f671cd5434809527981baab89b28ad', options)
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
+  fetch(
+    `https://us1.locationiq.com/v1/search/structured?street=${street1Name}&city=${cityName}&county=${countryName}&format=json&limit=1&key=${LOCATIONIQ_KEY}`,
+    options
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const coordinates = { lat: data[0].lat, lon: data[0].lon };
 
-  console.log({ enteredAddress });
+      const map = L.map("map").setView([coordinates.lat, coordinates.lon], 13); // Default to London
+      // Add OpenStreetMap tiles
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: "© OpenStreetMap contributors",
+      }).addTo(map);
+
+      // Add a marker to the map
+      const marker = L.marker([coordinates.lat, coordinates.lon]).addTo(map);
+
+      // Add a popup to the marker
+      marker.bindPopup("<b>Hello!</b><br>This is a Leaflet map.").openPopup();
+    })
+    .catch((err) => {
+      console.error(err);
+      throw new Error("could not fetch");
+    });
 }
 
 form.addEventListener("submit", searchAddressHandler);
